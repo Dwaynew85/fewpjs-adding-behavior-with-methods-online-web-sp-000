087d195bc14e6a2bfd6754a7dd3d69eaa251498b
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-13 17:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class Bird {
   }
 
   speak() {
-    if (this.sex === mail) return `It's me! ${this.name}, the parrot!`;
-    if (this.sex === femail) return `${this.name} says squarwk!`;
+    if (this.sex === male) return `It's me! ${this.name}, the parrot!`;
+    if (this.sex === female) return `${this.name} says squarwk!`;
   }
 }
